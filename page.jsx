6505d082a6ea6f7bd69cd544a6532decb625b18f
oussama894 <73111914+oussama894@users.noreply.github.com>
2023-09-15 14:24:03 +0100

    oussama

--- page.jsx
+++ page.jsx
@@ -1,15 +1,27 @@
+// pages/api/cart/[user].js
+
 import { sql } from "@vercel/postgres";
 
-export default async function Cart({ params }) {
-  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
-
+// Define a Server Component
+export default function Cart({ message }) {
   return (
     <div>
-      {rows.map((row) => (
-        <div key={row.id}>
-          {row.id} - {row.quantity}
-        </div>
-      ))}
+      <p>{message}</p>
     </div>
   );
+}
+
+// Define a loader function to insert user data into the "users" table
+export async function loader({ query }) {
+  const { username, password } = query;
+
+  try {
+    // Connect to the PostgreSQL database
+    await sql`INSERT INTO users (username, password) VALUES (${username}, ${password})`;
+
+    return { message: 'تم حفظ بيانات المستخدم بنجاح.' };
+  } catch (error) {
+    console.error(error);
+    return { message: 'حدث خطأ أثناء حفظ بيانات المستخدم.' };
+  }
 }
